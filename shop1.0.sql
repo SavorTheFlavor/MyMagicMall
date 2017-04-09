ee139d5982f2281e4f260215c9b4e62babcd11c8
SavorTheFlavor <18026165641@163.com>
2017-04-09 15:47:33 +0800

    忘了另一个sql文件，简洁版的...

---
+++ shop1.0.sql
@@ -0,0 +1,206 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : haha
+Source Server Version : 50520
+Source Host           : localhost:3306
+Source Database       : shop
+
+Target Server Type    : MYSQL
+Target Server Version : 50520
+File Encoding         : 65001
+
+Date: 2017-04-09 15:35:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for adminuser
+-- ----------------------------
+DROP TABLE IF EXISTS `adminuser`;
+CREATE TABLE `adminuser` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of adminuser
+-- ----------------------------
+INSERT INTO `adminuser` VALUES ('2', 'admin', 'admin');
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `cname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES ('1', '女装男装');
+INSERT INTO `category` VALUES ('2', '鞋靴箱包');
+INSERT INTO `category` VALUES ('3', '运动户外');
+INSERT INTO `category` VALUES ('4', '珠宝配饰');
+INSERT INTO `category` VALUES ('5', '手机数码');
+INSERT INTO `category` VALUES ('6', '家电办公');
+INSERT INTO `category` VALUES ('7', '护肤彩妆');
+INSERT INTO `category` VALUES ('10', '家居饰品');
+
+-- ----------------------------
+-- Table structure for categorysecond
+-- ----------------------------
+DROP TABLE IF EXISTS `categorysecond`;
+CREATE TABLE `categorysecond` (
+  `csid` int(11) NOT NULL AUTO_INCREMENT,
+  `csname` varchar(255) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`csid`),
+  KEY `FK936FCAF21DB1FD15` (`cid`),
+  CONSTRAINT `FK936FCAF21DB1FD15` FOREIGN KEY (`cid`) REFERENCES `category` (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of categorysecond
+-- ----------------------------
+INSERT INTO `categorysecond` VALUES ('1', '潮流女装', '1');
+INSERT INTO `categorysecond` VALUES ('2', '初冬羽绒', '1');
+INSERT INTO `categorysecond` VALUES ('3', '毛呢大衣', '1');
+INSERT INTO `categorysecond` VALUES ('4', '温暖毛衣', '1');
+INSERT INTO `categorysecond` VALUES ('5', '精选男装', '1');
+INSERT INTO `categorysecond` VALUES ('6', '冬季外套', '1');
+INSERT INTO `categorysecond` VALUES ('7', '羽绒服', '1');
+INSERT INTO `categorysecond` VALUES ('9', '女鞋', '2');
+INSERT INTO `categorysecond` VALUES ('10', '短靴', '2');
+INSERT INTO `categorysecond` VALUES ('11', '男鞋', '2');
+INSERT INTO `categorysecond` VALUES ('12', '女包', '2');
+INSERT INTO `categorysecond` VALUES ('13', '男包', '2');
+INSERT INTO `categorysecond` VALUES ('14', '服饰配件', '2');
+INSERT INTO `categorysecond` VALUES ('15', '运动鞋', '3');
+INSERT INTO `categorysecond` VALUES ('16', '运动服', '3');
+INSERT INTO `categorysecond` VALUES ('17', '户外运动', '3');
+INSERT INTO `categorysecond` VALUES ('18', '健身装备', '3');
+INSERT INTO `categorysecond` VALUES ('19', '骑行装备', '3');
+INSERT INTO `categorysecond` VALUES ('20', '珠宝首饰', '4');
+INSERT INTO `categorysecond` VALUES ('21', '时尚饰品', '4');
+INSERT INTO `categorysecond` VALUES ('22', '品质手表', '4');
+INSERT INTO `categorysecond` VALUES ('23', '眼镜配饰', '4');
+INSERT INTO `categorysecond` VALUES ('24', '手机', '5');
+INSERT INTO `categorysecond` VALUES ('25', '平板', '5');
+INSERT INTO `categorysecond` VALUES ('29', '厨房电器', '6');
+INSERT INTO `categorysecond` VALUES ('30', '生活电器', '6');
+INSERT INTO `categorysecond` VALUES ('31', '个户电器', '6');
+INSERT INTO `categorysecond` VALUES ('32', '办公耗材', '6');
+INSERT INTO `categorysecond` VALUES ('33', '美容护肤', '7');
+INSERT INTO `categorysecond` VALUES ('34', '强效保养', '7');
+INSERT INTO `categorysecond` VALUES ('35', '超值彩妆', '7');
+INSERT INTO `categorysecond` VALUES ('36', '换季保养', '7');
+INSERT INTO `categorysecond` VALUES ('40', '组合柜222', '10');
+
+-- ----------------------------
+-- Table structure for orderitem
+-- ----------------------------
+DROP TABLE IF EXISTS `orderitem`;
+CREATE TABLE `orderitem` (
+  `itemid` int(11) NOT NULL AUTO_INCREMENT,
+  `count` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemid`),
+  KEY `FKE8B2AB6166C01961` (`oid`),
+  KEY `FKE8B2AB6171DB7AE4` (`pid`),
+  KEY `FKE8B2AB6140ACF87A` (`oid`),
+  CONSTRAINT `FKE8B2AB6140ACF87A` FOREIGN KEY (`oid`) REFERENCES `orders` (`oid`),
+  CONSTRAINT `FKE8B2AB6171DB7AE4` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orderitem
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `oid` int(11) NOT NULL AUTO_INCREMENT,
+  `total` double DEFAULT NULL,
+  `ordertime` datetime DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `addr` varchar(50) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`oid`),
+  KEY `FKC3DF62E5AA3D9C7` (`uid`),
+  CONSTRAINT `FKC3DF62E5AA3D9C7` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9005 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(255) DEFAULT NULL,
+  `market_price` double DEFAULT NULL,
+  `shop_price` double DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `pdesc` varchar(255) DEFAULT NULL,
+  `is_hot` int(11) DEFAULT NULL,
+  `pdate` datetime DEFAULT NULL,
+  `csid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `FKED8DCCEFB9B74E02` (`csid`),
+  CONSTRAINT `FKED8DCCEFB9B74E02` FOREIGN KEY (`csid`) REFERENCES `categorysecond` (`csid`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+INSERT INTO `product` VALUES ('1', '日版连衣裙', '558', '228', 'products/14871-kWVIEF.jpg', '双11限量200件，拍完下架，加车享优惠，早下手早发货。。秋冬个性中长款毛衣，美丽和温度同在！限量供应，拒绝撞衫！迫于纱线和人工在不断上涨的双重压力下，产品涨价在即！少量现货出售中，手快有，手慢等哦，赶紧抢哦，绝对高大上。', '1', '2017-04-09 14:30:39', '1');
+INSERT INTO `product` VALUES ('2', '日版连衣裙', '558', '228', 'products/14871-kWVIEF.jpg', '双11限量200件，拍完下架，加车享优惠，早下手早发货。。秋冬个性中长款毛衣，美丽和温度同在！限量供应，拒绝撞衫！迫于纱线和人工在不断上涨的双重压力下，产品涨价在即！少量现货出售中，手快有，手慢等哦，赶紧抢哦，绝对高大上。', '1', '2017-04-09 14:30:39', '2');
+INSERT INTO `product` VALUES ('3', '水果套餐', '238', '2', 'products/0tq2a29-0-lp.jpg', '还没过期....', '0', '2017-04-09 15:32:31', null);
+INSERT INTO `product` VALUES ('77', '黑色制服', '434', '2', 'products/1297306392-NmFCja.jpg', '黑色..........', '1', '2017-04-09 15:05:41', '3');
+INSERT INTO `product` VALUES ('78', '初音服装', '9999', '999', 'products/14558-XdE065.jpg', '上衣是灰色的，领带是代表色葱绿，袖子是黑色加葱绿条纹，短裙是黑色加葱绿条纹，还有黑色加葱绿条纹的长袜.....', '1', '2017-04-09 15:08:07', '1');
+INSERT INTO `product` VALUES ('79', '葡萄', '2', '1', 'products/1-160r61532135c.jpg', '葡萄', '0', '2017-04-09 15:08:51', '1');
+INSERT INTO `product` VALUES ('80', '旧衣服', '0.1', '0.01', 'products/cs50009.png', '', '0', '2017-04-09 15:10:34', '3');
+INSERT INTO `product` VALUES ('82', '旧衣服', '0.1', '0.01', 'products/cs50009.png', '', '0', '2017-04-09 15:10:34', '3');
+INSERT INTO `product` VALUES ('84', '蝴蝶', '2222', '3333', 'products/22-140G109201CD.jpg', '蝴蝶在翩跹....', '1', '2017-04-09 15:33:23', null);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `addr` varchar(255) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `code` varchar(64) DEFAULT NULL,
+  `balance` double(44,6) DEFAULT NULL,
+  `coupon` int(12) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('7', 'aaa', 'aaa', '沈晶秉', 'aaa@shop.com', '15726607618', '南极', '1', null, '42.000000', '0');
+INSERT INTO `user` VALUES ('8', 'bbb', 'bbb', '沃姚库', 'bbb@shop.com', '18726607618', '月球', '1', '', '111.000000', '1');
+SET FOREIGN_KEY_CHECKS=1;
